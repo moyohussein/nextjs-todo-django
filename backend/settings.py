1b6a99b08b468debf741fee151e5f578955ea242
Hussein AbdulQohar <aqmhussein@gmail.com>
2021-08-16 19:53:39 +0100

    add db host

--- backend/settings.py
+++ backend/settings.py
@@ -91,6 +91,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
