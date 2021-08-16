99788d7ec72c542323fda38f195cbe24e1b75be5
Hussein AbdulQohar <aqmhussein@gmail.com>
2021-08-16 19:56:40 +0100

    set debug to true

--- backend/settings.py
+++ backend/settings.py
@@ -26,8 +26,8 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY', '^jv736r-y^w(4)m1x&*j&ee7w^c_n0xt##w7poa!zbwfckftg5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = True
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
